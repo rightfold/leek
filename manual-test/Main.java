8f6525d0e3af6cfa6144dad385c99858042bec0c
rightfold <rightfold@gmail.com>
2018-06-18 12:42:19 +0200

    Pass explicit charset to OutputStreamWriter constructor

--- manual-test/Main.java
+++ manual-test/Main.java
@@ -2,6 +2,9 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 import leek.rt.TextualTracer;
 import leek.rt.Tracer;
 
@@ -13,7 +16,8 @@ public final class Main
 
     public static void main(String[] args) throws IOException
     {
-        Writer writer = new OutputStreamWriter(System.out);
+        Charset charset = StandardCharsets.UTF_8;
+        Writer writer = new OutputStreamWriter(System.out, charset);
         Tracer tracer = new TextualTracer(writer);
         boolean result = example.INSTANCE.invoke(tracer, false, false, false);
         writer.flush();
